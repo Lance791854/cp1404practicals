2fa8775b2443e03649a518c5910680977a1a2fa7
lance <lance.howell@my.jcu.edu.au>
2023-06-18 21:25:47 +1000

    Prac 3 Completed work

---
+++ prac_03/randoms.py
@@ -0,0 +1,14 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+# The smallest number i could have seen is 5 and the largest is 20
+
+print(random.randrange(3, 10, 2))  # line 2
+# The smallest number i could have seen is 3 and the largest is 9
+# Line 2 can not produce a 4
+
+print(random.uniform(2.5, 5.5))  # line 3
+# The smallest number i could have seen is 2.5 and the largest is 5.5
+
+
+print(random.randint(1, 100))
